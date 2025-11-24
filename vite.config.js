21ad46af574f7c56f4533aaf32d98075deb4ae6a
Jesus Gutierrez <jesusdavid1004@gmail.com>
2025-11-23 19:43:03 -0500

    feat: enhance CI/CD pipeline and update Vite configuration for improved deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
+    export default defineConfig({
+        plugins: [vue()],
+        base: '/prueba-vue-ci/', // Cambia esto por el nombre de tu repositorio
+        resolve: {
+            alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom'
+    } 
 })
